4935c7d386b87e305a8229d2926fdf1bbd9eab6b
adoumadje <adoumadjecaleb@gmail.com>
2022-07-12 18:02:03 +0300

    last commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 
 function App() {
+  const counter = useSelector((state) => state.counter)
+  const dispatch = useDispatch()
+
+  function increment() {
+    dispatch({ type: 'INC' })
+  }
+
+  function decrement() {
+    dispatch({ type: 'DEC' })
+  }
+
+  function addBy() {
+    dispatch({ type: 'ADD', payload: 10 })
+  }
+
   return (
-    <div>App</div>
+    <div>
+      <h1>Counter App</h1>
+      <h2>{counter}</h2>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+      <button onClick={addBy}>Add by 10</button>
+    </div>
   )
 }
 
--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,23 @@
 import { createStore } from "redux";
 
 const reducerFn = (state={counter:0}, action) => {
+    // conditions of reducer actions:
+    // Synchronous Function
+    // We should not mutate the original state
 
+    if(action.type === 'INC') {
+        return { counter: state.counter + 1 }
+    }
+
+    if(action.type === 'DEC') {
+        return { counter: state.counter - 1 }
+    }
+
+    if(action.type === 'ADD') {
+        return { counter: state.counter + action.payload }
+    }
+
+    return state
 }
 
 const store = createStore(reducerFn)
